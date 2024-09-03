62387dd01d2122e6b0e766b1eea59312313a9a80
Zhiyi Ding <palisade0401@gmail.com>
2024-09-02 22:44:20 -0700

    Exercises 2.1.-2.5.

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,52 @@
+import { courses } from "./components/courseData";
+
+const Header = ({ course }) => {
+  return <h1>{course.name}</h1>;
+};
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  );
+};
+
+const Content = ({ course }) => {
+  return (
+    <div>
+      {course.parts.map((part) => (
+        <Part key={part.id} part={part} />
+      ))}
+    </div>
+  );
+};
+
+const Total = ({ course }) => {
+  const totalExercises = course.parts.reduce((sum, part) => {
+    return sum + part.exercises;
+  }, 0);
+  return <b>total of {totalExercises} exercises</b>;
+};
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div>
+      {courses.map((course) => (
+        <Course key={course.id} course={course} />
+      ))}
+    </div>
+  );
+};
+
+export default App;
